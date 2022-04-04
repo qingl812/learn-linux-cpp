88fa8075591a5a07f583b36e8199d5cc05dbb039
qingl <412753863@qq.com>
2022-04-04 11:34:03 +0800

    inline-assembly

--- inline-assembly/first.c
+++ inline-assembly/first.c
@@ -1,7 +1,37 @@
 /**
  * @brief 内联汇编
+ *
  * @details
- * ./first.md
+ * 汇编语言以两种形式出现：英特尔风格和 AT&T 风格。
+ * GNU C 编译器，即 GCC 使用 AT&T 语法，这就是我们将使用的。
+ *
+ * 1. 寄存器命名：
+ *      寄存器名称以 % 为前缀， 因此寄存器为 %eax、%cl 等，
+ *      而不仅仅是 eax、cl。
+ * 2. 操作数的顺序：
+ *      与 Intel 约定（第一个操作数是目标）不同，
+ *      操作数的顺序是源在前，目标在后。
+ *      例如，英特尔语法“mov eax, edx”在 AT&T 汇编中看起来像“mov %edx, %eax”。
+ * 3. 操作数大小：
+ *      在 AT&T 语法中，内存操作数的大小由操作码名称的最后一个字符确定。
+ *      后缀是 b 表示（8 位）字节，w 表示（16 位）字，l 表示（32 位）长。
+ *      例如，上述指令的正确语法应该是“movl %edx, %eax”。
+ * 4. 立即数操作数：
+ *      立即数操作数用 $ 前缀标记，如“addl $5, %eax”，
+ *      表示将立即数长值 5 添加到寄存器 %eax）。
+ * 5. 内存操作数：
+ *      缺少操作数前缀表示它是内存地址；
+ *      因此 "movl $bar, %ebx" 将变量 bar 的地址放入寄存器 %ebx，
+ *      而 "movl bar, %ebx" 将变量 bar 的内容放入寄存器 %ebx。
+ * 6. 索引：
+ *      索引或间接通过将索引寄存器或间接存储单元地址括在括号中来完成。
+ *      例如，“movl 8(%ebp), %eax”
+ *      （将 %ebp 指向的单元格偏移 8 处的内容移动到寄存器 %eax 中）。
+ *
+ * https://www.codeproject.com/Articles/15971/Using-Inline-Assembly-in-C-C
+ *
+ * @note
+ * gcc inline-assembly/first.c -o out/a.out && out/a.out
  */
 
 #include <stdio.h>
@@ -14,49 +44,66 @@ float cosx(float degree);
 float square_root(float val);
 
 int main() {
-    // example 0
+#define example_0 1
+#define example_1 1
+#define example_2 1
+#define example_3 1
+#define example_4 1
+#define example_5 1
+#define example_6 0
+#define example_7 0
 
-    /* Add 10 and 20 and store result into register %eax */
-    __asm__("movl $10, %eax;"
-            "movl $20, %ebx;"
-            "addl %ebx, %eax;");
+// example 0
+#if example_0
+    {
+        /* Add 10 and 20 and store result into register %eax */
+        __asm__("movl $10, %eax;"
+                "movl $20, %ebx;"
+                "addl %ebx, %eax;");
 
-    /* Subtract 20 from 10 and store result into register %eax */
-    __asm__("movl $10, %eax;"
-            "movl $20, %ebx;"
-            "subl %ebx, %eax;");
+        /* Subtract 20 from 10 and store result into register %eax */
+        __asm__("movl $10, %eax;"
+                "movl $20, %ebx;"
+                "subl %ebx, %eax;");
 
-    /* Multiply 10 and 20 and store result into register %eax */
-    __asm__("movl $10, %eax;"
-            "movl $20, %ebx;"
-            "imull %ebx, %eax;");
+        /* Multiply 10 and 20 and store result into register %eax */
+        __asm__("movl $10, %eax;"
+                "movl $20, %ebx;"
+                "imull %ebx, %eax;");
+    }
+#endif // !example_0
 
-    // example 1
-    int val;
-    asm("movl %%eax, %0;" : "=r"(val));
-    printf("example 1: %d\n", val);
-    /***
-     * 在本例中，变量“val”保存在寄存器中，
-     * 寄存器 eax 中的值被复制到该寄存器中，
-     * “val”的值从该寄存器更新到内存中。
-     * 当指定“r”约束时，
-     * gcc 可以将变量保存在任何可用的通用寄存器中。
-     * 我们还可以通过使用特定的寄存器约束直接指定寄存器名称。
-     * 寄存器约束如下：
-     * +---+--------------------+
-     * | r |    Register(s)     |
-     * +---+--------------------+
-     * | a |   %eax, %ax, %al   |
-     * | b |   %ebx, %bx, %bl   |
-     * | c |   %ecx, %cx, %cl   |
-     * | d |   %edx, %dx, %dl   |
-     * | S |   %esi, %si        |
-     * | D |   %edi, %di        |
-     * +---+--------------------+
-     */
+// example 1
+#if example_1
+    {
+        int val;
+        asm("movl %%eax, %0;" : "=r"(val));
+        printf("example 1: %d\n", val);
+        /***
+         * 在本例中，变量“val”保存在寄存器中，
+         * 寄存器 eax 中的值被复制到该寄存器中，
+         * “val”的值从该寄存器更新到内存中。
+         * 当指定“r”约束时，
+         * gcc 可以将变量保存在任何可用的通用寄存器中。
+         * 我们还可以通过使用特定的寄存器约束直接指定寄存器名称。
+         * 寄存器约束如下：
+         * +---+--------------------+
+         * | r |    Register(s)     |
+         * +---+--------------------+
+         * | a |   %eax, %ax, %al   |
+         * | b |   %ebx, %bx, %bl   |
+         * | c |   %ecx, %cx, %cl   |
+         * | d |   %edx, %dx, %dl   |
+         * | S |   %esi, %si        |
+         * | D |   %edi, %di        |
+         * +---+--------------------+
+         */
+    }
+#endif // !example_1
 
-    // example 2
-    int no = 100;
+// example 2
+#if example_2
+    int val, no = 100;
     asm("movl %1, %%ebx;"
         "movl %%ebx, %0;"
         : "=r"(val) /* output */
@@ -74,19 +121,26 @@ int main() {
      * 第三个冒号后被破坏的寄存器 %ebx 通知 GCC %ebx 的值将在 "asm" 中修改，
      * 因此 GCC 不会使用该寄存器来存储任何其他值。
      */
+#endif // !example_2
 
-    // example 3
-    int arg1 = 1, arg2 = 2, add;
-    __asm__("addl %%ebx, %%eax;" : "=a"(add) : "a"(arg1), "b"(arg2));
-    printf("example 3: %d %d %d\n", arg1, arg2, add);
-    /***
-     * 这里的“add”是寄存器 eax 引用的输出操作数。
-     * arg1 和 arg2 分别是寄存器 eax 和 ebx 引用的输入操作数。
-     * 让我们看一个使用扩展内联汇编语句的完整示例。
-     * 它对整数操作数执行简单的算术运算并显示结果（在下载中作为算术.c 提供）。
-     */
+// example 3
+#if example_3
+    {
+        int arg1 = 1, arg2 = 2, add;
+        __asm__("addl %%ebx, %%eax;" : "=a"(add) : "a"(arg1), "b"(arg2));
+        printf("example 3: %d %d %d\n", arg1, arg2, add);
+        /***
+         * 这里的“add”是寄存器 eax 引用的输出操作数。
+         * arg1 和 arg2 分别是寄存器 eax 和 ebx 引用的输入操作数。
+         * 让我们看一个使用扩展内联汇编语句的完整示例。
+         * 它对整数操作数执行简单的算术运算并显示结果（在下载中作为算术.c
+         * 提供）。
+         */
+    }
+#endif // !example_3
 
-    // example 4
+// example 4
+#if example_4
     {
         int arg1, arg2, add, sub, mul, quo, rem;
 
@@ -113,8 +167,10 @@ int main() {
         printf("%d / %d = %d\n", arg1, arg2, quo);
         printf("%d %% %d = %d\n", arg1, arg2, rem);
     }
+#endif // !example_4
 
-    // exapmle 5
+// example 5
+#if example_5
     /***
      * 如果我们的汇编语句必须在我们放置的地方执行，
      * （即不能作为优化移出循环），
@@ -124,11 +180,15 @@ int main() {
      */
     // 请参考以下示例，
     // 该示例使用众所周知的欧几里得算法（被誉为第一算法）计算最大公约数。
-    int first = 6, second = 9;
-    printf("example 5:\n");
-    printf("GCD of %d & %d is %d\n", first, second, gcd(first, second));
+    {
+        int first = 6, second = 9;
+        printf("example 5:\n");
+        printf("GCD of %d & %d is %d\n", first, second, gcd(first, second));
+    }
+#endif // !example_5
 
-    // exapmle 6
+// example 6
+#if example_6
     // 下面是更多使用 FPU（浮点单元）指令集的示例。
     // 执行简单浮点运算的示例程序：
     {
@@ -172,19 +232,27 @@ int main() {
         printf("%f * %f = %f\n", arg1, arg2, mul);
         printf("%f / %f = %f\n", arg1, arg2, div);
     }
+#endif // !example_6
 
-    // exapmle 7
+// example 7
+#if example_7
     // 计算三角函数（如 sin 和 cos）的示例程序：
-    float theta;
-    printf("Enter theta in degrees : ");
-    scanf("%f", &theta);
+    {
+        float theta;
+        printf("Enter theta in degrees : ");
+        scanf("%f", &theta);
 
-    printf("sinx(%f) = %f\n", theta, sinx(theta));
-    printf("cosx(%f) = %f\n", theta, cosx(theta));
-    printf("square_root(%f) = %f\n", theta, square_root(theta));
+        printf("sinx(%f) = %f\n", theta, sinx(theta));
+        printf("cosx(%f) = %f\n", theta, cosx(theta));
+        printf("square_root(%f) = %f\n", theta, square_root(theta));
+    }
+#endif // !example_7
 
     return 0;
 }
+
+// example 4
+#if example_4
 
 int gcd(int a, int b) {
     int result;
@@ -204,6 +272,10 @@ int gcd(int a, int b) {
 
     return result;
 }
+#endif // !example_4
+
+// example 7
+#if example_7
 
 float sinx(float degree) {
     float result, two_right_angles = 180.0f;
@@ -248,3 +320,4 @@ float square_root(float val) {
                          : "g"(val));
     return result;
 }
+#endif // !example_7
